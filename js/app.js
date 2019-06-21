cee8541ea61b900e56d44663d8de5a393ed073f0
Jordan Balagot <jbalagot@ayzenberg.com>
2019-06-21 14:23:15 -0700

    add original punctuation

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
 $(document).ready(function(){
-	var i = 1;
+	var i = 2;
 	$('.monologueenter').text(localStorage.monologue);
 	if(typeof(localStorage.monologue) != 'undefined')
 	{
-		var sentences = localStorage.monologue.split(/[.|\!|?]/);
+		var sentences = localStorage.monologue.split(/([.|\!|?])/);
 	}else
 	var sentences = '';
-	if(typeof(sentences[0]) != 'undefined')
+	if(typeof(sentences[0]) != 'undefined' && typeof(sentences[1]) != 'undefined')
 	{
-		$('.monologue').append(sentences[0] + ". ");
+		$('.monologue').append(sentences[0] + sentences[1] + ' ');
 	}
 
 	$('button.save').click(function(){
 		localStorage.monologue = $('.monologueenter').val();
-		sentences = localStorage.monologue.split(/[.|\!|?]/);
-		i = 1;
-		if(typeof(sentences[0]) != 'undefined')
+		sentences = localStorage.monologue.split(/([.|\!|?])/);
+		i = 2;
+		if(typeof(sentences[0]) != 'undefined' && sentences[1] != 'undefined')
 		{
-			$('.monologue').text('').append(sentences[0] + ". ");
+			$('.monologue').text('').append(sentences[0] + sentences[1] + " ");
 		}
 		$('.savemessage').text("Saved, now tap on text below");
 	});
@@ -29,27 +29,27 @@ $(document).ready(function(){
 
 	$('.monologue').click(function(){
 		var monologue = localStorage.monologue.replace(/(?:\r\n|\r|\n)/g, '<br>');
-		sentences = monologue.split(/[.|\!|?]/);
-		if(typeof(sentences[i]) != 'undefined')
+		sentences = monologue.split(/([.|\!|?])/);
+		if(typeof(sentences[i]) != 'undefined' && typeof(sentences[i + 1] != 'undefined'))
 		{
-			$('.monologue').append(sentences[i] + ". ");
-			i++;
+			$('.monologue').append(sentences[i] + sentences[i + 1] + ' ');
+			i+=2;
 		}else
 		{
-			i = 0;
+			i = 2;
 			$('.monologue').text('');
 		}
 
 	});
 
 	$('button.restart').click(function(){
-		i = 1;
+		i = 2;
 		$('.monologue').text('');
 		var monologue = localStorage.monologue.replace(/(?:\r\n|\r|\n)/g, '<br>');
-		sentences = monologue.split(/[.|\!|?]/);
-		if(typeof(sentences[0]) != 'undefined')
+		sentences = monologue.split(/([.|\!|?])/);
+		if(typeof(sentences[0]) != 'undefined' && typeof(sentences[1]) != 'undefined')
 		{
-			$('.monologue').text('').append(sentences[0] + ". ");
+			$('.monologue').text('').append(sentences[0] + sentences[1] + " ");
 		}
 	});
 });
